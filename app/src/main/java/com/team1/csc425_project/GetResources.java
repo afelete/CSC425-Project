535b703075a6beeb27e31a12fe88266980c16d1e
kw83028 <kw83028@yahoo.com>
2016-02-17 20:39:34 -0500

    The buttons work!!!

--- app/src/main/java/com/team1/csc425_project/GetResources.java
+++ app/src/main/java/com/team1/csc425_project/GetResources.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 public class GetResources extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_resources);
     }
--- app/src/main/java/com/team1/csc425_project/MainActivity.java
+++ app/src/main/java/com/team1/csc425_project/MainActivity.java
@@ -1,13 +1,43 @@
 package com.team1.csc425_project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button resourceButton = (Button) findViewById(R.id.resourceButton);
+        resourceButton.setOnClickListener(this);
+        Button donateButton = (Button) findViewById(R.id.donateButton);
+        donateButton.setOnClickListener(this);
+        Button volunteerButton = (Button) findViewById(R.id.volunteerButton);
+        volunteerButton.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+
+        if (v.getId() == R.id.resourceButton) {
+            Intent intent = new Intent(v.getContext(), GetResources.class);
+            startActivity(intent);
+
+        } else if (v.getId() == R.id.donateButton) {
+            Intent intent = new Intent(v.getContext(), Donate.class);
+            startActivity(intent);
+
+        } else if (v.getId() == R.id.volunteerButton) {
+            Intent intent = new Intent(v.getContext(), Volunteer.class);
+            startActivity(intent);
+        }
+
+
     }
 }
+
+
