047e721b0b8f627e70ce63a06cee3cb97912ae42
kw83028 <kw83028@yahoo.com>
2016-02-22 21:11:45 -0500

    Maps Buttons Work on the Donate Page!!!!

--- app/src/main/java/com/team1/csc425_project/Donate.java
+++ app/src/main/java/com/team1/csc425_project/Donate.java
@@ -1,7 +1,10 @@
 package com.team1.csc425_project;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Donate extends AppCompatActivity {
 
@@ -9,5 +12,32 @@ public class Donate extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donate);
+    }
+
+    public void process(View view)
+    {
+        Intent intent=null, chooser=null;
+
+        if(view.getId()==R.id.donateMapButton1)
+        {
+            intent=new Intent(android.content.Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("geo:43.023624,-83.69435?q=43.023624,-83.69435"));
+            chooser=Intent.createChooser(intent,"Launch Maps");
+            startActivity(chooser);
+        }
+        if(view.getId()==R.id.donateMapButton2)
+        {
+            intent=new Intent(android.content.Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("geo:43.006097,-83.686161?q=43.006097,-83.686161"));
+            chooser=Intent.createChooser(intent,"Launch Maps");
+            startActivity(chooser);
+        }
+        if(view.getId()==R.id.donateMapButton3)
+        {
+            intent=new Intent(android.content.Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("geo:43.008456,-83.666178?q=43.008456,-83.666178"));
+            chooser=Intent.createChooser(intent,"Launch Maps");
+            startActivity(chooser);
+        }
     }
 }
