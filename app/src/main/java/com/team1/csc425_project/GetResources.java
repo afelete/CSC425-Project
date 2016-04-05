0b86ac9838ff7ceb54addab5696952fcc41f70e9
kw83028 <kw83028@yahoo.com>
2016-04-05 19:49:38 -0400

    Get Resources page working

--- app/src/main/java/com/team1/csc425_project/GetResources.java
+++ app/src/main/java/com/team1/csc425_project/GetResources.java
@@ -1,7 +1,10 @@
 package com.team1.csc425_project;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class GetResources extends AppCompatActivity {
 
@@ -9,5 +12,46 @@ public class GetResources extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_resources);
+    }
+
+    public void process(View view)
+    {
+        Intent intent=null, chooser=null;
+
+        if(view.getId()==R.id.resourceMapButton1)
+        {
+            intent=new Intent(android.content.Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("geo:43.012776,-83.683700?q=43.012776,-83.683700"));
+            chooser=Intent.createChooser(intent,"Launch Maps");
+            startActivity(chooser);
+        }
+        if(view.getId()==R.id.resourceMapButton2)
+        {
+            intent=new Intent(android.content.Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("geo:43.029870,-83.703023?q=43.029870,-83.703023"));
+            chooser=Intent.createChooser(intent,"Launch Maps");
+            startActivity(chooser);
+        }
+        if(view.getId()==R.id.resourceMapButton3)
+        {
+            intent=new Intent(android.content.Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("geo:43.045331,-83.654912?q=43.045331,-83.654912"));
+            chooser=Intent.createChooser(intent,"Launch Maps");
+            startActivity(chooser);
+        }
+        if(view.getId()==R.id.resourceMapButton4)
+        {
+            intent=new Intent(android.content.Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("geo:43.061347,-83.714771?q=43.061347,-83.714771"));
+            chooser=Intent.createChooser(intent,"Launch Maps");
+            startActivity(chooser);
+        }
+        if(view.getId()==R.id.resourceMapButton5)
+        {
+            intent=new Intent(android.content.Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("geo:42.988741,-83.672190?q=42.988741,-83.672190"));
+            chooser=Intent.createChooser(intent,"Launch Maps");
+            startActivity(chooser);
+        }
     }
 }
